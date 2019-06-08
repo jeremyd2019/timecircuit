855eae7e2bb518baeef8da086a2ab702f090b49e
Jeremy Drake <github@jdrake.com>
2019-06-07 21:53:49 -0700

    Add US DST rule to compile-time timezone offset.
    Tomohiko Sakamoto's algorithm for day of week is used to determine the
    DST transition days.
    
    Note that 1:00 through 1:59 am on the first Sunday in November is always
    interpreted as the "first" occurance (1:59 is followed by 1:00, not
    2:00), as there is no way to tell which it really is.  2:00 through 2:59
    on the second Sunday in March should never occur, but if for some reason
    it does it would be interpreted as DST ON.

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -6,7 +6,22 @@
 #include <time.h>
 #include "compile_time.h"
 
-// rough estimate of DST
-#define TIMEZONE_OFFSET_SECS (((__TIME_MONTH__ < 3 || __TIME_MONTH__ >= 11) ? -8 : -7) * SEC_PER_HOUR)
+// Tomohiko Sakamoto's algorithm for day of week, with slight stylistic tweaks for use in macro
+#define GET_DOW(y,m,d) (((y-(m<3))+(y-(m<3))/4-(y-(m<3))/100+(y-(m<3))/400+*("-bed=pen+mad."+m)+d)%7)
+
+#define IS_USA_DST ( \
+    (__TIME_MONTH__ > 3 || (__TIME_MONTH__ == 3 && __TIME_DAYS__ > 8+(7-GET_DOW(__TIME_YEARS__,3,8))%7) || \
+        (__TIME_MONTH__ == 3 && __TIME_DAYS__ == 8+(7-GET_DOW(__TIME_YEARS__,3,8))%7 && __TIME_HOURS__ >= 2)) && \
+    (__TIME_MONTH__ < 11 || (__TIME_MONTH__ == 11 && __TIME_DAYS__ < 1+(7-GET_DOW(__TIME_YEARS__,11,1))%7) || \
+        (__TIME_MONTH__ == 11 && __TIME_DAYS__ == 1+(7-GET_DOW(__TIME_YEARS__,11,1))%7 && __TIME_HOURS__ < 2)) \
+)
+
+#define EST5EDT -5
+#define CST6CDT -6
+#define MST7MDT -7
+#define PST8PDT -8
+
+#define TIMEZONE_OFFSET_SECS ((PST8PDT + IS_USA_DST) * SEC_PER_HOUR)
+
 extern const uint32_t INITIAL_TIME PROGMEM;
 const uint32_t INITIAL_TIME PROGMEM = UNIX_TIMESTAMP - TIMEZONE_OFFSET_SECS - UNIX_OFFSET;
